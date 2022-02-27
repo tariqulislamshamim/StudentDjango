d7c235a2492193b7f52cfbb462feb2c9b5d912c8
Tariqul Islam <tariqul1911228@acps.edu.bd>
2022-02-27 23:42:06 +0600

    fix db

--- StudentDjango/settings.py
+++ StudentDjango/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^(y_dv@)h5t5%!cc+tb2ia#&of#%3&r&am2k(y=j5+2wu!+(vx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['acpsattendance.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -150,7 +150,7 @@ SESSION_COOKIE_SECURE = True #to avoid transmitting the session cookie over HTTP
 
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
-#SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 86400  # 1 day
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
